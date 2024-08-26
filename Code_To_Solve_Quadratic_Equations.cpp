f06ff19bb872d63c4e3bcc534c587dd910cb465e
Jigomas <104555528+Jigomas@users.noreply.github.com>
2024-08-26 16:03:29 +0300

    Add files via upload

--- Code_To_Solve_Quadratic_Equations.cpp
+++ Code_To_Solve_Quadratic_Equations.cpp
@@ -1,19 +1,25 @@
 /**
-* @param a is a First_Coefficient  of the equation
-* @param b is a Second_Coefficient of the equation
-* @param c is a Third_Coefficient  of the equation
-*
-* @param x1 is a First_Root  of the equation
-* @param x2 is a Second_Root of the equation
-*
-* @return Number_Is_Received - checking if received string is a number
-* @return Inputs_Of_Numbers - makes a massive of received numbers
-* @return Inputs_Of_Numbers - makes a massive of received numbers
-* @return Solve_Linear_Equation - solves equation if First_Coefficient is 0
-* @return Solve_Quadric_Equation - solves equation if First_Coefficient is not 0
-* @return Discr_Is_Positive - solves equation with 2 roots //todo all functions
-
+//---------------------------------------------------------
+//* @param a is a First_Coefficient  of the equation
+//* @param b is a Second_Coefficient of the equation
+//* @param c is a Third_Coefficient  of the equation
+//*
+//* @param x1 is a First_Root  of the equation
+//* @param x2 is a Second_Root of the equation
+//*
+//* @return Number_Is_Received - checking if received string is a number
+//* @return Inputs_Of_Numbers - makes a massive of received numbers
+//* @return Inputs_Of_Numbers - makes a massive of received numbers
+//* @return Solve_Linear_Equation - solves equation if First_Coefficient is 0
+//* @return Solve_Quadric_Equation - solves equation if First_Coefficient is not 0
+//* @return Solve_If_Discr_Is_Positive - solves equation with 2 roots
+//* @return Solve_If_Discr_Is_Zero - solves equation with 1 root
+//* @return All_Tests - contains all coefficients of tests
+//* @return Test_With_Structures - runs tests with premade Structures
+//* @return Test - runs tests without premade Structures
+//---------------------------------------------------------
 */
+
 
 
 #include <assert.h>
@@ -21,7 +27,7 @@
 #include <math.h>
 #include <stdio.h>
 
-const int SMALL_DIFF  = 1e-8;
+const int SMALL_DIFF  = 1e-6;
 
 enum Different_Amount_Of_Roots_By_Exit_Codes
 {
@@ -32,7 +38,6 @@ enum Different_Amount_Of_Roots_By_Exit_Codes
     QUADRATIC_EQUATION_ENDLESS_ROOTS =  3,
 };
 
-
 enum All_Possible_Exit_Codes
 {
     SOMETHING_WENT_WRONG      = -1,
@@ -40,19 +45,20 @@ enum All_Possible_Exit_Codes
     ALL_FINISHED_SUCCESFULLY  =  1,
 };
 
-
-
-//#if !defined(Online)
-//#define Online
-
-//int Function_That_Runs_Tests()
-
 struct Types_Of_Coefficients
 {
     double a, b, c;
     double x1_Expected, x2_Expected;
     int Amount_Of_Roots_Expected;
 };
+
+struct All_Needed_Numbers
+{
+    double a, b, c;
+    double x1, x2;
+};
+
+
 
 int Solving_Quadratic_Or_Linear_Equation(int *Current_Code_Of_Program);
 
@@ -71,55 +77,62 @@ int Solve_Linear_Equation(double Second_Coefficient, double Third_Coefficient,
                           double *Root_1);
 
 int Solve_Quadric_Equation(double First_Coefficient, double Second_Coefficient,
-                          double Third_Coefficient, double *Root_1,
-                          double *Root_2);
-//todo rename Discr
-void Discr_Is_Positive(double Discr, double First_Coefficient,
-                       double Second_Coefficient, double Third_Coefficient,
-                       double *Root_1, double *Root_2);
+                           double Third_Coefficient, double *Root_1,
+                           double *Root_2);
 
-int Discr_Is_Zero(double First_Coefficient, double Second_Coefficient,
+int Solve_If_Discr_Is_Positive(double Discr, double First_Coefficient,
+                               double Second_Coefficient, double Third_Coefficient,
+                               double *Root_1, double *Root_2);
+
+int Solve_If_Discr_Is_Zero(double First_Coefficient, double Second_Coefficient,
                    double *First_Root);
 
 void All_Tests();
 
 void Test_With_Structures(Types_Of_Coefficients *Taken_Data, double *x1_Test, double *x2_Test);
 
-
 void Test(double First_Coefficient, double Second_Coefficient,
              double Third_Coefficient, double Root_1_Expected,
              double Root_2_Expected, int Amount_Of_Roots_Expected);
+
+int Is_Close_To_Zero(double Number);
+//--------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------
 
 
 
 int main()
 {
     double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
-    //do while (Program_Return_Codes != IN_PROGRESSSION)
     int Current_Code_Of_Program = PROGRAM_STILL_IN_PROGRESS;
-    int Amount_Of_Roots_Of_Quadric_Equation = 0;
-    Amount_Of_Roots_Of_Quadric_Equation = Solving_Quadratic_Or_Linear_Equation(&Current_Code_Of_Program);
-    Return_Codes_Of_Program(x1, x2,Amount_Of_Roots_Of_Quadric_Equation);
+    int Amount_Of_Roots_Of_Quadric_Equation = -1;
+    Amount_Of_Roots_Of_Quadric_Equation =
+                Solving_Quadratic_Or_Linear_Equation(&Current_Code_Of_Program);
 }
 
 
 int Solving_Quadratic_Or_Linear_Equation(int *Current_Code_Of_Program)
 {
     int Amount_Of_Roots_Of_Quadric_Equation = 0;
+
     while (*Current_Code_Of_Program == PROGRAM_STILL_IN_PROGRESS)
-        {
-            double a = 0, b = 0, c = 0, x1 = 0, x2 = 0; // todo to struct
-            Inputs_Of_Numbers(&a, &b, &c);
-            int Amount_Of_Roots_Of_Quadric_Equation = QUADRATIC_EQUATION_ERROR;
+    {
+        double a = 0;
+        double b = 0;
+        double c = 0;//todo struct
+        double x1 = 0;
+        double x2 = 0;
+        Inputs_Of_Numbers(&a, &b, &c);
+        int Amount_Of_Roots_Of_Quadric_Equation = QUADRATIC_EQUATION_ERROR;
+        if ( 0 != fabs(a) )
+            Amount_Of_Roots_Of_Quadric_Equation = Solve_Quadric_Equation(a, b, c, &x1, &x2);
+        else
+            Amount_Of_Roots_Of_Quadric_Equation = Solve_Linear_Equation(b, c, &x1);
+        *Current_Code_Of_Program = Return_Codes_Of_Program(x1, x2,
+        Amount_Of_Roots_Of_Quadric_Equation);
+    }
 
-            if ( 0 != fabs(a) )
-                Amount_Of_Roots_Of_Quadric_Equation = Solve_Quadric_Equation(a, b, c, &x1, &x2);
-
-            else
-                Amount_Of_Roots_Of_Quadric_Equation = Solve_Linear_Equation(b, c, &x1);
-
-            *Current_Code_Of_Program = Return_Codes_Of_Program(x1, x2, Amount_Of_Roots_Of_Quadric_Equation);
-        }
     return Amount_Of_Roots_Of_Quadric_Equation;
 }
 
@@ -127,38 +140,31 @@ int Solving_Quadratic_Or_Linear_Equation(int *Current_Code_Of_Program)
 int Return_Codes_Of_Program(double x1, double x2, int Amount_Of_Roots_Of_Quadric_Equation)
 {
     switch (Amount_Of_Roots_Of_Quadric_Equation)
-        {
-            //  ENDLESS_ROOTS = 3, TWO_ROOTS = 2, ONE_ROOT = 1, NO_ROOTS = 0, ERROR = -1
-            case QUADRATIC_EQUATION_ENDLESS_ROOTS:
-                printf("Endless amount of roots\n");
-
-                return ALL_FINISHED_SUCCESFULLY;
-                break;
-
-            case QUADRATIC_EQUATION_TWO_ROOTS:
-                printf("Root_1 = %lg, Root_2 = %lg, Amount_Of_Roots_Of_Quadric_Equation = %d\n", x1, x2, Amount_Of_Roots_Of_Quadric_Equation);
-
-                return ALL_FINISHED_SUCCESFULLY;
-                break;
-
-            case QUADRATIC_EQUATION_ONE_ROOT:
-                printf("Root = %lg, Amount_Of_Roots_Of_Quadric_Equation = %d\n", x1, Amount_Of_Roots_Of_Quadric_Equation);
-
-                return ALL_FINISHED_SUCCESFULLY;
-                break;
-
-            case QUADRATIC_EQUATION_NO_ROOTS:
-                printf("Quadratic equation have no roots\n");
-
-                return ALL_FINISHED_SUCCESFULLY;
-                break;
-
-            default:
-                printf("ERROR: No_Exit_Code_Received");
-
-                return SOMETHING_WENT_WRONG;
-                break;
-        }
+    {
+        //  ENDLESS_ROOTS = 3, TWO_ROOTS = 2, ONE_ROOT = 1, NO_ROOTS = 0, ERROR = -1
+        case QUADRATIC_EQUATION_ENDLESS_ROOTS:
+            printf("Endless amount of roots\n\n");
+            return ALL_FINISHED_SUCCESFULLY;
+            break;
+        case QUADRATIC_EQUATION_TWO_ROOTS:
+            printf("Root_1 = %lg, Root_2 = %lg, Amount_Of_Roots_Of_Quadric_Equation = %d\n\n",
+             x1, x2, Amount_Of_Roots_Of_Quadric_Equation);
+            return ALL_FINISHED_SUCCESFULLY;
+            break;
+        case QUADRATIC_EQUATION_ONE_ROOT:
+            printf("The_Only_Root = %lg, Amount_Of_Roots_Of_Quadric_Equation = %d\n\n", x1,
+             Amount_Of_Roots_Of_Quadric_Equation);
+            return ALL_FINISHED_SUCCESFULLY;
+            break;
+        case QUADRATIC_EQUATION_NO_ROOTS:
+            printf("Quadratic equation have no roots\n\n");
+            return ALL_FINISHED_SUCCESFULLY;
+            break;
+        default:
+            printf("ERROR: No_Exit_Code_Received\n\n");
+            return SOMETHING_WENT_WRONG;
+            break;
+    }
 }
 
 
@@ -184,6 +190,7 @@ int Number_Is_Received(double *Input_Numbers, int i)
 
         Clear_Buf(Current_Symbol);
     }
+
     return 0;
 }
 
@@ -197,6 +204,7 @@ int Check_String_Empty(int Current_Symbol, int Result)
         if (isspace(Current_Symbol) == 0)
             Result = 0;
     }
+
     return Result;
 }
 
@@ -205,9 +213,8 @@ int Check_String_Empty(int Current_Symbol, int Result)
 void Clear_Buf(int Current_Symbol)
 {
     while (Current_Symbol != '\n' && Current_Symbol != EOF)
-    {
         Current_Symbol = getchar();
-    }
+
     return;
 }
 
@@ -224,7 +231,8 @@ void Inputs_Of_Numbers(double *a, double *b, double *c)
 
     double Input_Numbers[3] = {0, 0, 0};
 
-    printf("\t\tPlease type 3 different numbers which will be a Coefficients of quadratic equation:\n");
+    printf("\t\tPlease type 3 different numbers which will be a "
+           "Coefficients of quadratic equation:\n");
 
     for (int i = 0; i < 3; i++)
     {
@@ -232,15 +240,14 @@ void Inputs_Of_Numbers(double *a, double *b, double *c)
         int Is_Number_Received = 0;
 
         while (Is_Number_Received == 0)
-        {
             Is_Number_Received = Number_Is_Received(&Input_Numbers[i],  i);
-        }
+
     }
 
     printf("I`ve got:\n");
 
-    for (int i = 0; i < 3; i++)
-        printf("Coefficient %d: %lf\n", i + 1, Input_Numbers[i]);
+    for (int i = 0; i < 3; i++)//todo const
+        printf("Coefficient %d: %lg\n", i + 1, Input_Numbers[i]);
 
     *a = Input_Numbers[0];
     *b = Input_Numbers[1];
@@ -251,12 +258,12 @@ void Inputs_Of_Numbers(double *a, double *b, double *c)
 
 int Solve_Linear_Equation(double b, double c, double *x1)
 {
-    if( (b > SMALL_DIFF) || (b < -SMALL_DIFF) )
+    if (Is_Close_To_Zero(b) != 0)
     {
         *x1 = (-c / b);
 
         return QUADRATIC_EQUATION_ONE_ROOT;
-     }
+    }
     else
     {
         return QUADRATIC_EQUATION_NO_ROOTS;
@@ -266,7 +273,7 @@ int Solve_Linear_Equation(double b, double c, double *x1)
 
 
 int Solve_Quadric_Equation(double a, double b,
-                          double c, double *x1, double *x2)
+                           double c, double *x1, double *x2)
 {
     double Discr = b * b - 4 * a * c;
 
@@ -274,33 +281,37 @@ int Solve_Quadric_Equation(double a, double b,
 
     if (Discr > SMALL_DIFF)
     {
-        Discr_Is_Positive(Discr, a, b, c, x1, x2);
-
-        return QUADRATIC_EQUATION_TWO_ROOTS;
+        if (Solve_If_Discr_Is_Positive(Discr, a, b, c, x1, x2))
+            return QUADRATIC_EQUATION_TWO_ROOTS;
     }
     else if (Discr < -SMALL_DIFF)
     {
         return QUADRATIC_EQUATION_ENDLESS_ROOTS;
     }
-    else
+    else if (Is_Close_To_Zero(Discr) == 1)
     {
-        if (Discr_Is_Zero(a, b, x1))
+        if (Solve_If_Discr_Is_Zero(a, b, x1))
             return QUADRATIC_EQUATION_ONE_ROOT;
     }
+
+    return QUADRATIC_EQUATION_ERROR;
 }
 
 
 
-void Discr_Is_Positive(double Discr, double a, double b, double c, double *x1, double *x2)
+int Solve_If_Discr_Is_Positive(double Discr, double a, double b, double c, double *x1, double *x2)
 {
     double Quadric_Root_From_Discr = sqrt(Discr);
+
     *x1 = (-b + Quadric_Root_From_Discr) / (2 * a);
     *x2 = (-b - Quadric_Root_From_Discr) / (2 * a);
+
+    return 1;
 }
 
 
 
-int Discr_Is_Zero(double a, double b, double *x1)
+int Solve_If_Discr_Is_Zero(double a, double b, double *x1)
 {
     *x1 =  (-b) / (2 * a);
 
@@ -309,18 +320,19 @@ int Discr_Is_Zero(double a, double b, double *x1)
 
 
 
-/*void All_Tests()
+void All_Tests()
 {
     const int AMOUNT_OF_TESTS = 2;
                                        //  a, b, c, x1, x2, Amount_Of_Roots_Of_Quadric_Equation
-    struct Types_Of_Coefficients Taken_Data [AMOUNT_OF_TESTS] = {{1, 2, 1, -1, 0, 1}, {2, 4, 2, -1, 0, 1}};
+    struct Types_Of_Coefficients Taken_Data [AMOUNT_OF_TESTS] = {{1, 2, 1, -1, 0, 1},
+                                                                 {2, 4, 2, -1, 0, 1}};
     for (int i = 0; i < AMOUNT_OF_TESTS; i++)
     {
         double x1_Test = 0, x2_Test = 0;
         Test_With_Structures(Taken_Data, &x1_Test, &x2_Test);
     }
 }
-*/
+
 
 
 void Test_With_Structures(Types_Of_Coefficients *data, double *x1_Test, double *x2_Test)
@@ -331,7 +343,7 @@ void Test_With_Structures(Types_Of_Coefficients *data, double *x1_Test, double *
 
 
 void Test(double a, double b, double c,
-             double x1_Expected, double x2_Expected, int Amount_Of_Roots_Expected)
+                        double x1_Expected, double x2_Expected, int Amount_Of_Roots_Expected)
 {
     static int Test_Number = 1;
     double x1 = 0;
@@ -345,11 +357,12 @@ void Test(double a, double b, double c,
         || (x2 > x2_Expected + SMALL_DIFF)
         || (x2 < x2_Expected - SMALL_DIFF) )
     {
-        printf("Test %d Failed: a = %lg, b = %lg, c = %lg, x1 = %lg, x2 = %lg,"
-            " Amount_Of_Roots_Of_Quadric_Equation = %d\n",Test_Number, a, b, c, x1, x2, Amount_Of_Roots_Of_Quadric_Equation);
+        printf("Test %d Failed: a = %lg, b = %lg, c = %lg, x1 = %lg, x2 = %lg, "
+                "Amount_Of_Roots_Of_Quadric_Equation = %d\n",Test_Number, a, b, c, x1, x2,
+                 Amount_Of_Roots_Of_Quadric_Equation);
 
         printf("Expected: x1 = %lg, x2 = %lg, Amount_Of_Roots_Of_Quadric_Equation = %d\n",
-        x1_Expected, x2_Expected, Amount_Of_Roots_Expected);
+                          x1_Expected, x2_Expected, Amount_Of_Roots_Expected);
     }
     else
     {
@@ -361,4 +374,15 @@ void Test(double a, double b, double c,
 
 
 
-//#endif
+int Is_Close_To_Zero(double Number)
+{
+    if (fabs(Number) <= SMALL_DIFF)//todo Ask why does <= but not the <
+        return 1;
+
+    else
+        return 0;
+}
+
+
+
+
